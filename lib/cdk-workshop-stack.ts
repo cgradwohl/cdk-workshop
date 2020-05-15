c17b7d0a8bb87e5fdf6997279ecf0185c09a5062
Chris Gradwohl <christophergradwohl@gmail.com>
2020-05-15 15:20:09 -0700

    attempt to create lambda paths with api gateway.

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -7,22 +7,67 @@ export class CdkWorkshopStack extends cdk.Stack {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
-    });
+    // const hello = new lambda.Function(this, 'HelloHandler', {
+    //   runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+    //   code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+    //   handler: 'hello.handler'                // file is "hello", function is "handler"
+    // });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     // new LambdaRestApi(scope: Construct, id: string, props: LambdaRestApiProps)
-    new apigateway.LambdaRestApi(
-      this, 
-      'Endpoint', 
+    // The following code defines a REST API that routes all requests to the specified AWS Lambda function
+    // new apigateway.LambdaRestApi(
+    //   this, 
+    //   'Endpoint', 
+    //   {
+    //     handler: hello,
+    //     proxy: true, // If true, route all requests to the Lambda Function. (optional, default: true)
+    //   }
+    // );
+
+    // const restaurantsHandler = new lambda.Function(
+    //   this,
+    //   'restaurantsHandler', 
+    //   {
+    //     runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+    //     code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+    //     handler: 'restaurants.get'                // file is "hello", function is "handler"
+    //   }
+    // );
+
+    const getIndex = new lambda.Function(this,
+      'getIndex', 
       {
-        handler: hello,
-        proxy: true, // If true, route all requests to the Lambda Function. (optional, default: true)
+        runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+        code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+        handler: 'index.get'                // file is "hello", function is "handler"
       }
     );
+
+    // define root /
+    const api = new apigateway.LambdaRestApi(
+      this, 
+      'restaurants-api', 
+      {
+        handler: getIndex,
+        proxy: false,
+      }
+    );
+
+    const geRestaurantsHandler = new lambda.Function(this,
+      'restaurantsHandler', 
+      {
+        runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+        code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+        handler: 'restaurants.get'                // file is "hello", function is "handler"
+      }
+    );
+    const getRestaurantsIntegration = new apigateway.LambdaIntegration(geRestaurantsHandler);
+    
+    // /restuarants
+    const restaurants = api.root.addResource('restaurants');
+    
+    restaurants.addMethod('GET', getRestaurantsIntegration);
 
   }
 }
